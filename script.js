b99c96e7317ab8e2cdfbbd4870efba17971b1f6f
Jonathan Mellergard John <jonathan.mellergard@gmail.com>
2019-12-30 22:15:13 +0100

    added checkGuess function
    added checkGuess function;
    added quiz complete message;

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
 let availableMultiplications = 
 ['2 * 7', '2 * 5', '2 * 8', '9 * 2', '6 * 2', '4 * 2',
- '4 * 5', '4 * 8', '4 * 4', '7 * 4', '9 * 4', '4 * 6',
+ /*'4 * 5', '4 * 8', '4 * 4', '7 * 4', '9 * 4', '4 * 6',
  '3 * 5', '3 * 7', '3 * 4', '8 * 3', '6 * 3', '9 * 3',
  '6 * 5', '6 * 9', '6 * 4', '7 * 6', '6 * 6', '8 * 6',
- '5 * 4', '5 * 9', '5 * 3', '7 * 5', '8 * 5', '5 * 5',
+ '5 * 4', '5 * 9', '5 * 3', '7 * 5', '8 * 5', '5 * 5',*/
  '7 * 8', '9 * 9', '8 * 8', '8 * 9', '7 * 7', '9 * 7'];
 
 let availableProducts =
-[14, 10, 16, 18, 12, 8, 
- 20, 32, 16, 28, 36, 24, 
+[14, 10, 16, 18, 12, 8,
+ /*20, 32, 16, 28, 36, 24,
  15, 21, 12, 24, 18, 27,
  30, 54, 24, 42, 36, 48,
- 20, 45, 15, 35, 40, 25, 
+ 20, 45, 15, 35, 40, 25,*/
  56, 81, 64, 72, 49, 63];
 
 let selector = 0;
 let question = 0;
 let answer = 0;
+let guess = 0;
+let wrongGuesses = [];
+let input = document.querySelector('.guessField');
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function selectQuestionAndAnswer() {
+    if (availableMultiplications.length == 0) {
+        if (wrongGuesses.length == 0) {
+            document.querySelector('.guessCorrectionMessage').innerHTML = 
+            'Färdig med diagnos! Du fick alla rätt, bra jobbat!' 
+        } else {            
+            document.querySelector('.guessCorrectionMessage').innerHTML = 
+            'Färdig med diagnos! Du fick ' + wrongGuesses.length + ' fel.'
+        }
+    } else {
     selector = getRandomNumber(0, (availableMultiplications.length - 1));
     question = availableMultiplications[selector];
     answer = availableProducts[selector];
+    }
 }
 
 function removeUsedQuestionsAndAnswers() {
@@ -39,12 +52,30 @@ function displayQuestion(){
     document.querySelector('.guessField').value ='';
 }
 
+function checkGuess() {
+    guess = Number(document.querySelector('.guessField').value);
+    if (guess !== answer) {
+        wrongGuesses.push(question);
+        document.querySelector('.guessCorrectionMessage').innerHTML = 'fel!';
+    } else {
+        document.querySelector('.guessCorrectionMessage').innerHTML = 'rätt!';   
+    }
+}
+
+input.addEventListener('keyup',function(e){
+    if (e.keyCode === 13) {
+    document.querySelector('.guessButton').click();
+  }
+});
+
 function startQuiz() {
     selectQuestionAndAnswer();
     removeUsedQuestionsAndAnswers();
     displayQuestion();
 }
+
 function continueQuiz() {
+    checkGuess();
     selectQuestionAndAnswer();
     removeUsedQuestionsAndAnswers();
     displayQuestion();
